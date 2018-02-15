7f87d8b8fd18e6ec8443188fe3a69dc8a4a5c726
Maciej Pawlik <m.pawlik@cyfronet.pl>
2018-02-15 15:51:00 +0100

    Add s3 upload.

--- credostorage/handler.py
+++ credostorage/handler.py
@@ -1,26 +1,25 @@
 import boto3
 import json
-import uuid
 import base64
 
 import config
 
 BUCKET_NAME = 'detections'
-s3 = boto3.resource(
-    's3',
-    endpoint_url=config.S3_ENDPOINT,
-    aws_access_key_id=config.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY
-)
 
 
 def upload(filename, data):
+    s3 = boto3.resource(
+        's3',
+        endpoint_url=config.S3_ENDPOINT,
+        aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY
+    )
     s3.Object(BUCKET_NAME, filename).put(Body=str(data))
-    print 'successful upload ' + filename
 
 
 def handle(body):
     # print 'recieved message: ' + body
     data = json.loads(body)
     img_data = base64.b64decode(data['body']['detection']['frame_content'])
-    upload(str(uuid.uuid4()) + ".png", data)
+    filename = data['body']['user_info']['key'] + '-' + data['body']['detection']['timestamp'] + '.png'
+    upload(filename, data)
